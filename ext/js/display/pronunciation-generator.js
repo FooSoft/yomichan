595f9ff7bfb0792c371d723eaf4e95484106693c
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2021-07-17 12:01:27 -0400

    Add a hidden handakuten for copy-paste purposes

--- ext/js/display/pronunciation-generator.js
+++ ext/js/display/pronunciation-generator.js
@@ -54,7 +54,11 @@ class PronunciationGenerator {
                 n1.dataset.nasal = 'true';
                 n1.dataset.originalText = mora;
                 n2.textContent = this._getPlainMora(mora);
-                const n3 = document.createElement('span');
+                let n3 = document.createElement('span');
+                n3.className = 'pitch-accent-character-nasal-diacritic';
+                n3.textContent = '\u309a'; // Combining handakuten
+                n1.appendChild(n3);
+                n3 = document.createElement('span');
                 n3.className = 'pitch-accent-character-nasal-indicator';
                 n1.appendChild(n3);
             }
