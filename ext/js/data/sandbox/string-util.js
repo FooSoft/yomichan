8145024b028fdcec646dd67cd758d87ea5dbe750
toasted-nutbread <toasted-nutbread@users.noreply.github.com>
2022-08-20 12:49:03 -0400

    Rename StringUtil to ArrayBufferUtil

--- ext/js/data/sandbox/string-util.js
+++ ext/js/data/sandbox/string-util.js
@@ -18,7 +18,7 @@
 /**
  * Class containing generic string utility functions.
  */
-class StringUtil {
+class ArrayBufferUtil {
     /**
      * Decodes the contents of an ArrayBuffer using UTF8.
      * @param {ArrayBuffer} arrayBuffer The input ArrayBuffer.
